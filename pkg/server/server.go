5a634f7bbf661273e3c5bede453e48f6669801aa
Sergey <xxsmotur@gmail.com>
2020-05-24 13:42:15 +0500

    Complete of test Loki integration

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,8 +1,11 @@
 package server
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 
+	"github.com/afiskon/promtail-client/promtail"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/jwtauth"
@@ -20,6 +23,25 @@ type Server struct {
 	log *logrus.Logger
 }
 
+func makePromtail(sourceName, jobName string) (promtail.Client, error) {
+	labels := "{source=\"" + sourceName + "\",job=\"" + jobName + "\"}"
+	conf := promtail.ClientConfig{
+		PushURL:            "http://loki:3100/api/prom/push",
+		Labels:             labels,
+		BatchWait:          5 * time.Second,
+		BatchEntriesNumber: 10000,
+		SendLevel:          promtail.INFO,
+		PrintLevel:         promtail.ERROR,
+	}
+
+	loki, err := promtail.NewClientJson(conf)
+	if err != nil {
+		return nil, fmt.Errorf("unable to init Promtail client: %v", err)
+	}
+
+	return loki, err
+}
+
 // Make provides making of server
 func Make(st *storage.Storage, p *config.Project) {
 	tokenAuth = jwtauth.New("HS256", []byte("secret"), nil)
@@ -27,15 +49,20 @@ func Make(st *storage.Storage, p *config.Project) {
 		db: st,
 	}
 	initPrometheus()
+	loki, err := makePromtail("test", "data")
+	if err != nil {
+		panic(err)
+	}
 	r := chi.NewRouter()
-	r.Handle("/metrics", promhttp.Handler())
 	r.Use(middleware.RequestID)
 	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
+	r.Handle("/metrics", promhttp.Handler())
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		totalRequests.Inc()
+		loki.Infof("source = %s time = %s\n", "server", time.Now().Format(time.RFC3339))
 		w.Write([]byte("welcome")) //nolint
 	})
 	r.Route("/users", func(r chi.Router) {
@@ -59,7 +86,7 @@ func Make(st *storage.Storage, p *config.Project) {
 		Addr:    p.Server.Address,
 		Handler: r,
 	})
-	p.Log.WithField("package", "server").Info("main: Starting the server")
+	p.Log.WithFields(logrus.Fields{"package": "server", "address": p.Server.Address}).Info("main: Starting the server")
 	if err := graceful.Graceful(server.ListenAndServe, server.Shutdown); err != nil {
 		p.Log.WithField("package", "server").Fatalln("main: Failed to gracefully shutdown")
 	}
